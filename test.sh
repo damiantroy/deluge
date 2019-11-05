501f3313385981901dca816a2e0499c8c210fde4
Damian Troy <github@black.hole.com.au>
2019-11-05 14:38:23 +1100

    Force test to use IPv4

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -x
 
-curl http://localhost:8112/ |grep '<title>Deluge: Web UI'
+curl http://127.0.0.1:8112/ |grep '<title>Deluge: Web UI'
 RESULT=$?
 
 if [[ ${RESULT} -eq 0 ]]; then
