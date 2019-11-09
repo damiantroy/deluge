fc66e54879b61d4c24fd1be0b922c8d107372e18
Damian Troy <github@black.hole.com.au>
2019-11-09 14:25:59 +1100

    Experimenting with testing

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+echo "*** Start debug"
+id
+ls -la /config
+echo "*** End debug"
+
 /usr/bin/deluged -c /config
 while ! nc -z 127.0.0.1 58846; do sleep 0.1; done
 /usr/bin/deluge-web -c /config
